e83191034d9a778ec57cbdcb1f78f0667fb0090a
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2019-03-10 17:28:46 -0500

    added okular

--- arch-i3-build.sh
+++ arch-i3-build.sh
@@ -2,7 +2,7 @@ build='pacman -Syyu --noconfirm i3-wm compton firefox chromium atom code python-
 build+=' font-mathematica zsh python-sympy mpv feh dunst ipython jupyter-notebook  networkmanager qbittorrent caprine ranger thunar git cmake zeal blueman'
 build+=' lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings network-manager-applet xscreensaver xorg-xrdb dmenu ttf-font-awesome task autocutsel'
 build+=' redshift apm bash-language-server scrot xbacklight alsa-utils imagemagick pulseaudio pulseaudio-alsa pulseaudio-bluetooth'
-
+build+=' phonon-qt5-gstreamer okular'
 eval $build
 
 systemctl enable lightdm.service
